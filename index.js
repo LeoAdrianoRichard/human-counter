cd06b4593b394ce2ab65a300c42b3df7bda4421d
LeoAdrianoRichard <1211103433@student.mmu.edu.my>
2024-11-14 12:02:54 +0800

    My first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let count=0;
+
+let countEL=document.getElementById("count-el");
+
+function increment(){
+    count=count+1;
+    countEL.innerText=count;
+}
+
+function save(){
+
+    let countStr=count + "-";
+    let message=document.getElementById("message-el")
+    message.textContent+= countStr;
+    count=0;
+    countEL.innerText=count;
+}
+//My first project
+
+
